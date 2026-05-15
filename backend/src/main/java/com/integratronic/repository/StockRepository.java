078ea827b6c0071a5c74f9700cd6d5d6a808a4d8
johantm98 <johan199891@gmail.com>
2026-05-15 14:52:19 +0200

    Implementa flujo de venta fisica

--- backend/src/main/java/com/integratronic/repository/StockRepository.java
+++ backend/src/main/java/com/integratronic/repository/StockRepository.java
@@ -1,8 +1,12 @@
 package com.integratronic.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.integratronic.model.Stock;
 
 public interface StockRepository extends JpaRepository<Stock, Integer> {
+
+    Optional<Stock> findByProducto_IdProducto(Integer idProducto);
 }
